3dce5488e658543cad4172d2ea48754035e420eb
spencerdennison <spencerdennison@proton.me>
2026-07-15 00:18:09 -0600

    fix(hardware-detection): look for any occurence of BAT

--- hardware-detection.sh
+++ hardware-detection.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 chassis_info=$(ls /sys/class/power_supply/ | grep "BAT" || true)
 gpu_info=$(lspci | grep -iE "vga|3d|display" || true)
 
-if [[ "$chassis_info" == "BAT*" ]]; then
+if [[ "$chassis_info" == *BAT* ]]; then
     printf "Chassis: Laptop\n"
 else
     printf "Chassis: Desktop\n"
