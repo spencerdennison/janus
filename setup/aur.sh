1009f50876ae4348204c58353a39585bfc6530e7
spencerdennison <spencerdennison@proton.me>
2026-07-16 23:29:15 -0600

    fix(setup/aur): spelling error for linux

--- setup/aur.sh
+++ setup/aur.sh
@@ -3,7 +3,7 @@
 if command -v paru > /dev/null; then
     log_skipping "paru AUR helper already installed"
 else
-    git clone https://aur.archliunx.org/paru.git /tmp/paru
+    git clone https://aur.archlinux.org/paru.git /tmp/paru
     cd /tmp/paru
     makepkg -si
     rm -rf /tmp/paru
