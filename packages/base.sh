5469911a8b24d1c24fd10958549a0f46259c0c1a
spencerdennison <spencerdennison@proton.me>
2026-07-16 22:29:59 -0600

    feat(packages/base): add base packages install wm de terminal etc

---
+++ packages/base.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [[ "$form_factor" == "laptop" ]]; then
+    sudo pacman -S bash-completion brightnessctl btop discord foot grim less mako man-db noto-fonts noto-fonts-emoji openssh slurp stow swappy sway tofi tree ttf-jetbrains-mono-nerd waybar wiremix wl-clipboard
+else
+    sudo pacman -S bash-completion btop discord foot grim less mako man-db noto-fonts noto-fonts-emoji openssh slurp stow swappy sway tofi tree ttf-jetbrains-mono-nerd waybar wiremix wl-clipboard
+fi
