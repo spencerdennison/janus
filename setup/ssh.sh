34f4b4c3c77517c110af981a22232e7cc573376e
spencerdennison <spencerdennison@proton.me>
2026-07-16 23:35:58 -0600

    fix(setup/ssh): true/false not conditional

--- setup/ssh.sh
+++ setup/ssh.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-if ! [[ ls ~/.ssh/ | grep "git" ]]; then
-    ssh-keygen -t ed25519 -f ~/.ssh/git
-else
+if ls ~/.ssh/ | grep "git"; then
     log_skipping "Git SSH key already exists"
-    continue
+else
+    ssh-keygen -t ed25519 -f ~/.ssh/git
 fi
