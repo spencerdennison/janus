15b38a792111671174982578900da7e06b2e2090
spencerdennison <spencerdennison@proton.me>
2026-07-16 23:26:54 -0600

    fix(setup/aur): not comparsion returns true/false

--- setup/aur.sh
+++ setup/aur.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if ! [[ command -v paru > /dev/null ]]; then
+if command -v paru > /dev/null; then
+    log_skipping "paru AUR helper already installed"
+else
     git clone https://aur.archliunx.org/paru.git /tmp/paru
     cd /tmp/paru
     makepkg -si
     rm -rf /tmp/paru
-else
-    log_skipping "paru AUR helper already installed"
 fi
