bb818fb505363c387213e39988959f41ccd44d85
spencerdennison <spencerdennison@proton.me>
2026-07-17 00:04:00 -0600

    chore(detect/system): refactor slightly

--- detect/system.sh
+++ detect/system.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-distro_info=$(cat /etc/os-release | grep "^ID")
-battery_info=$(ls /sys/class/power_supply/ | grep -i "BAT" || true)
+distro=$(cat /etc/os-release | grep "^ID")
+battery=$(ls /sys/class/power_supply/ | grep -i "BAT" || true)
 
-if [[ "$distro_info" == *arch* ]]; then
+if [[ "$distro" == *arch* ]]; then
     distro=arch
     log_info "Distro: $distro"
 else
     log_error "janus only supports Arch Linux currently"
-    log_info "exiting..."
     exit 1
 fi
 
-if [[ "$battery_info" ]]; then
+if [[ -n "$battery" ]]; then
     form_factor="laptop"
 else
     form_factor="desktop"
