e4da3500556aa8bd2b92a7f6eda197e8d1507910
spencerdennison <spencerdennison@proton.me>
2026-07-16 22:28:35 -0600

    feat(config/dotfiles): add dotfiles configuration setup

---
+++ config/dotfiles.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if ! [[ "ls ~/ | grep ".config"" ]]; then
+    rm -rf .bashrc
+    rm -rf .bash_profile
+    mkdir .config
+    git clone git@github.com:$git_username/dotfiles.git
+    cd ~/dotfiles
+    stow .
+else
+    log_skipping "dotfiles repo already cloned"
+fi
