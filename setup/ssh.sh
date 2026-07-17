ec185762769943f27ec76cec38cc62c8d59f5022
spencerdennison <spencerdennison@proton.me>
2026-07-17 15:56:09 -0600

    fix(setup/ssh): wrap ls call in conditional statement

--- setup/ssh.sh
+++ setup/ssh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if ls ~/.ssh/ | grep "git"; then
+if [[ "ls ~/.ssh/ | grep "git" || true" ]]; then
     log_skipping "Git SSH key already exists"
 else
     ssh-keygen -t ed25519 -f ~/.ssh/git
