7c17fe7f23dafebdc3212313957056fc60ee978a
spencerdennison <spencerdennison@proton.me>
2026-07-18 19:04:43 -0600

    chore(config/git): remove gitconfig check/creation, assumed this is kept in configuration files

--- config/git.sh
+++ config/git.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-if ! [[ "ls ~/ | grep ".gitconfig"" ]]; then 
-    read -p "Enter GitHub username: " "$git_username"
-    read -p "Enter GitHub email: " "$git_email"
-
-    git config --global user.email "$git_email"
-    git config --global user.name "$git_username"
-else
-    log_skipping "gitconfig creation"
-fi
-
 read -p "Setup GitHub authentication? [Y/n]" $git_option
 case "$git_option" in
     [yY]|"") 
