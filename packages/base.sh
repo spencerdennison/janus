f6dce9a43b5e853f0cc8e65f5793151284e59d4f
spencerdennison <spencerdennison@proton.me>
2026-07-21 10:32:56 -0600

    refactor(packages/base): openssh installed elsewhere

--- packages/base.sh
+++ packages/base.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ "$form_factor" == "laptop" ]]; then
-    paru -S bash-completion brightnessctl btop discord foot grim less mako man-db noto-fonts noto-fonts-emoji openssh slurp stow swappy sway tree ttf-jetbrains-mono-nerd waybar wiremix wl-clipboard --noconfirm
+    paru -S bash-completion brightnessctl btop discord foot grim less mako man-db noto-fonts noto-fonts-emoji slurp stow swappy sway tree ttf-jetbrains-mono-nerd waybar wiremix wl-clipboard --noconfirm
 else
-    paru -S bash-completion btop discord foot grim less mako man-db noto-fonts noto-fonts-emoji openssh slurp stow swappy sway tree ttf-jetbrains-mono-nerd waybar wiremix wl-clipboard --noconfirm
+    paru -S bash-completion btop discord foot grim less mako man-db noto-fonts noto-fonts-emoji slurp stow swappy sway tree ttf-jetbrains-mono-nerd waybar wiremix wl-clipboard --noconfirm
 fi
