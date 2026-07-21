4c390863263b70b9d020260fb89ab171c82da407
spencerdennison <spencerdennison@proton.me>
2026-07-21 10:15:33 -0600

    feat(packages/gaming): add optional install

--- packages/gaming.sh
+++ packages/gaming.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-if [[ "$form_factor" == "desktop" ]]; then
-    paru -S steam gamescope --nonconfirm
-else
-    log_skipping "gaming packages not needed"
-fi
+read -p "Install gaming packages? [Y/n] " gaming_option
+
+case "$gaming_option" in
+    [yY]|"")
+        paru -S steam gamescope --nonconfirm
+        ;;
+
+    [nN])
+        log_skipping "gaming packages"
+        ;;
+
+    *)
+        log_error "invalid input"
+        ;;
+esac
