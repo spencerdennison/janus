bd62849bcf66c89768b026c85064c67680c7a510
spencerdennison <spencerdennison@proton.me>
2026-07-21 10:21:32 -0600

    fix(setup/dotfiles): use -f flag for actual file check

--- setup/dotfiles.sh
+++ setup/dotfiles.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if ! [[ "ls ~/ | grep ".config"" ]]; then
+if [[ -f ~/.config ]]; then
+    log_skipping "dotfiles repo clone"
+else
     rm -rf .bashrc
     rm -rf .bash_profile
     mkdir .config
     git clone git@github.com:$git_username/dotfiles.git
     cd ~/dotfiles
     stow .
-else
-    log_skipping "dotfiles repo clone"
 fi
