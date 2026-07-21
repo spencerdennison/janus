cfe3907fafda40abeed0a3e4f44312d92fdb2745
spencerdennison <spencerdennison@proton.me>
2026-07-21 11:16:07 -0600

    fix(setup/dotfiles): give full path to bash configs

--- setup/dotfiles.sh
+++ setup/dotfiles.sh
@@ -3,8 +3,8 @@
 if [[ -e ~/dotfiles ]]; then
     log_skipping "dotfiles repo clone"
 else
-    rm -rf .bashrc
-    rm -rf .bash_profile
+    rm -f ~/.bashrc
+    rm -f ~/.bash_profile
     git clone https://github.com/spencerdennison/dotfiles.git ~/dotfiles
     cd ~/dotfiles
     stow .
