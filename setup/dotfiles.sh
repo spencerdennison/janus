e9756d97ad4e21019c33bf64d73cb43e90a674f3
spencerdennison <spencerdennison@proton.me>
2026-07-21 10:36:44 -0600

    fix(setup/dotfiles): change folder check to dotfiles

--- setup/dotfiles.sh
+++ setup/dotfiles.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-if [[ -f ~/.config ]]; then
+if [[ -e ~/dotfiles ]]; then
     log_skipping "dotfiles repo clone"
 else
     rm -rf .bashrc
     rm -rf .bash_profile
-    mkdir .config
-    git clone git@github.com:$git_username/dotfiles.git
+    git clone git@github.com:spencerdennison/dotfiles.git
     cd ~/dotfiles
     stow .
 fi
