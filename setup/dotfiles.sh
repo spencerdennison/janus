8fb9196b45e356da1f6c383c8737469c6e092564
spencerdennison <spencerdennison@proton.me>
2026-07-21 11:12:00 -0600

    fix(setup/dotfiles): clone dotfiles to home directory

--- setup/dotfiles.sh
+++ setup/dotfiles.sh
@@ -5,7 +5,7 @@ if [[ -e ~/dotfiles ]]; then
 else
     rm -rf .bashrc
     rm -rf .bash_profile
-    git clone https://github.com/spencerdennison/dotfiles.git
+    git clone https://github.com/spencerdennison/dotfiles.git ~/dotfiles
     cd ~/dotfiles
     stow .
 fi
