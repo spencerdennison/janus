3f3b9cd6e1587f08c3cb8ad059b48f76fb838cba
spencerdennison <spencerdennison@proton.me>
2026-08-09 17:06:59 -0600

    refactor(packages/base): use pacman instead of paru

--- packages/base.sh
+++ packages/base.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 if [[ "$form_factor" == "laptop" ]]; then
-    paru -S bash-completion brightnessctl btop discord foot grim less mako man-db noto-fonts noto-fonts-emoji slurp stow swappy sway tree ttf-jetbrains-mono-nerd waybar wiremix wl-clipboard --noconfirm
+    sudo pacman -S bash-completion brightnessctl btop discord foot grim less mako man-db noto-fonts noto-fonts-emoji slurp stow swappy sway tree ttf-jetbrains-mono-nerd waybar wiremix wl-clipboard xdg-desktop-portal-gtk xdg-desktop-portal-wlr
 else
-    paru -S bash-completion btop discord foot grim less mako man-db noto-fonts noto-fonts-emoji slurp stow swappy sway tree ttf-jetbrains-mono-nerd waybar wiremix wl-clipboard --noconfirm
+    sudo pacman -S bash-completion btop discord foot grim less mako man-db noto-fonts noto-fonts-emoji slurp stow swappy sway tree ttf-jetbrains-mono-nerd waybar wiremix wl-clipboard  xdg-desktop-portal-gtk xdg-desktop-portal-wlr
+
 fi
