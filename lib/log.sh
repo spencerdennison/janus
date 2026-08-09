ebf02261526568d29e614332cb7a90d6f2a95ab1
spencerdennison <spencerdennison@proton.me>
2026-08-09 16:39:52 -0600

    chore(lib/log): change info color white --> blue

--- lib/log.sh
+++ lib/log.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-WHITE="\033[0;37m"
+BLUE="\033[0;34m"
 GREEN="\033[0;32m"
 RED="\033[0;31m"
 RESET="\033[0m"
 
 # Logging functions
 log_info() {
-    printf "${WHITE}[INFO]${RESET} %s\n" "$1"
+    printf "${BLUE}[INFO]${RESET} %s\n" "$1"
 }
 
 log_success() {
