c2e03dd4b86ade94296e65a8d80114778de73158
spencerdennison <spencerdennison@proton.me>
2026-08-09 17:07:36 -0600

    refactor(packages/drivers): use pacman instead of paru

--- packages/drivers.sh
+++ packages/drivers.sh
@@ -2,15 +2,15 @@
 
 case $gpu_vendor in
     "amd")
-        paru -S lib32-mesa lib32-vulkan-radeon mesa vulkan-radeon --noconfirm
+        sudo pacman -S lib32-mesa lib32-vulkan-radeon mesa vulkan-radeon
         ;;
 
     "nvidia")
-        paru -S lib32-nvidia-utils nvidia-open-dkms --noconfirm
+        sudo pacman -S lib32-nvidia-utils nvidia-open-dkms
         ;;
 
     "intel")
-        paru -S lib32-mesa lib32-vulkan-intel mesa vulkan-intel --noconfirm
+        sudo pacman -S lib32-mesa lib32-vulkan-intel mesa vulkan-intel
         ;;
 
     "none" | "unknown")
