ee85a086889b048a1a7468a1f9b9ecbc14c940d7
spencerdennison <spencerdennison@proton.me>
2026-08-09 22:24:27 -0600

    refactor(packages/base): add --noconfirm flag

--- packages/base.sh
+++ packages/base.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [[ "$form_factor" == "laptop" ]]; then
-    sudo pacman -S bash-completion brightnessctl btop discord foot grim less mako man-db noto-fonts noto-fonts-emoji slurp stow swappy sway tree ttf-jetbrains-mono-nerd waybar wiremix wl-clipboard xdg-desktop-portal-gtk xdg-desktop-portal-wlr
+    sudo pacman -S bash-completion brightnessctl btop discord foot grim less mako man-db noto-fonts noto-fonts-emoji slurp stow swappy sway tree ttf-jetbrains-mono-nerd waybar wiremix wl-clipboard xdg-desktop-portal-gtk xdg-desktop-portal-wlr --noconfirm
 else
-    sudo pacman -S bash-completion btop discord foot grim less mako man-db noto-fonts noto-fonts-emoji slurp stow swappy sway tree ttf-jetbrains-mono-nerd waybar wiremix wl-clipboard  xdg-desktop-portal-gtk xdg-desktop-portal-wlr
+    sudo pacman -S bash-completion btop discord foot grim less mako man-db noto-fonts noto-fonts-emoji slurp stow swappy sway tree ttf-jetbrains-mono-nerd waybar wiremix wl-clipboard  xdg-desktop-portal-gtk xdg-desktop-portal-wlr --noconfirm
 
 fi
