d5234cfb6fd120df4be81079b834da9a07e5caa7
spencerdennison <spencerdennison@proton.me>
2026-08-09 19:16:06 -0600

    refactor(setup/ssh): use environment variables instead of ~

--- setup/ssh.sh
+++ setup/ssh.sh
@@ -2,8 +2,8 @@
 
 sudo pacman -S openssh
 
-if [[ -e "~/.ssh/git" ]]; then
+if [[ -e "$HOME/.ssh/git" ]]; then
     log_skipping "Git SSH key creation"
 else
-    ssh-keygen -t ed25519 -f ~/.ssh/git
+    ssh-keygen -t ed25519 -f "$HOME/.ssh/git"
 fi
