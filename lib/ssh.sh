7ff94ec3471c978819fb2d18530ca4a7cdda780f
spencerdennison <spencerdennison@proton.me>
2026-08-10 21:42:34 -0600

    fix(lib/ssh): add dir creation before ssh-keygen

--- lib/ssh.sh
+++ lib/ssh.sh
@@ -5,7 +5,8 @@ log_info "starting: ssh.sh"
 if [[ -e "$HOME/.ssh/git" ]]; then
     log_skipping "Git SSH key creation: already exists"
 else
-    ssh-keygen -t ed25519 -filename "$HOME/.ssh/git"
+    mkdir -p "$HOME/.ssh"
+    ssh-keygen -t ed25519 -f "$HOME/.ssh/git"
 fi
 
 log_info "finished: ssh.sh"
