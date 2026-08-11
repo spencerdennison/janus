88bed3c537843ea630df50e7b934c0f09857dbcc
spencerdennison <spencerdennison@proton.me>
2026-08-10 21:11:01 -0600

    chore(lib/aur): move with other files to /lib

---
+++ lib/aur.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+log_info "starting: paru.sh"
+
+if command -v paru > /dev/null; then
+    log_skipping "paru install: already exists"
+else
+    if [[ -e /tmp/paru ]]; then
+        log_info "removing /tmp/paru: "
+        rm --force --recursive /tmp/paru
+    fi
+
+    git clone https://aur.archlinux.org/paru.git /tmp/paru
+    cd /tmp/paru
+    makepkg --syncdeps --install
+    rm --force --recursive /tmp/paru
+fi
+
+paru --sync librewolf-hellfire-bin material-black-colors-theme tofi xcursor-hackneyed-light --noconfirm
+
+log_info "finished: paru.sh"
